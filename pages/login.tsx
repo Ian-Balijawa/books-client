b15d2f407585a7947512b383208bad7db8601cde
ian <ianbalijawa16@gmail.com>
2022-05-03 19:45:54 +0300

    add login component

---
+++ pages/login.tsx
@@ -0,0 +1,9 @@
+import { FunctionComponent } from 'react';
+import { App } from '../components/Login/App';
+interface LoginProps {}
+
+const Login: FunctionComponent<LoginProps> = () => {
+	return <App />;
+};
+
+export default Login;
